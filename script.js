1436318d3945ca2ee97c3d61baa5f3c670004cab
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-05-17 13:41:13 +0530

    create ticket js complete

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const addBtn = document.querySelector(".add-btn");
 const modalCont = document.querySelector(".modal-cont");
+const allPriorityColors = document.querySelectorAll(".priority-color");
+let colors = ['lightpink', 'lightgreen', 'lightblue', 'black'];
+let modalPriorityColor = colors[colors.length-1]; // black is selected
+let textAreaCont = document.querySelector(".textarea-cont");
+
+const mainCont = document.querySelector(".main-cont");
+
+
+
 
 let isModalPresent = false;
-
 addBtn.addEventListener('click', function(){
     if(!isModalPresent){
         modalCont.style.display = "flex"; //modal add ho jayega screen par
@@ -16,9 +24,8 @@ addBtn.addEventListener('click', function(){
     isModalPresent = !isModalPresent // toggling  effect  insted using line 9 & 13 we use toggling effect;
 })
 
-// code smajna hai firse 
 
-const allPriorityColors = document.querySelectorAll(".priority-color");
+
 
 allPriorityColors.forEach(function (colorElement) {
   colorElement.addEventListener("click", function () {
@@ -28,5 +35,33 @@ allPriorityColors.forEach(function (colorElement) {
     });
     //add active class on click elevnt list on element.
     colorElement.classList.add("active");
+    modalPriorityColor = colorElement.classList[0];
   });
 });
+
+modalCont.addEventListener("keydown", function(e){
+    let key = e.key;
+    if(key == "Shift"){
+        console.log(modalPriorityColor);
+        console.log(textAreaCont.value);
+        createTicket(modalPriorityColor, textAreaCont.value);
+        modalCont.style.display = "none";
+        isModalPresent = false;
+        textAreaCont.value = "";
+        allPriorityColors.forEach(function(colorElem){
+            colorElem.classList.remove("active");
+        })
+    }
+});
+
+function createTicket(ticketColor, data){
+    let ticketCont = document.createElement("div"); //<div></div>
+    ticketCont.setAttribute("class", "ticket-cont");
+    ticketCont.innerHTML=`
+        <div class="ticket-color ${ticketColor}"></div>
+        <div class="ticket-id" ></div>
+        <div class="ticket-area">${data}</div>
+    `;
+
+    mainCont.appendChild(ticketCont);
+}
